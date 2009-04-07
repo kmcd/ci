441d93d7ad4819c63fcc6b8623fae4aae5667126
Keith McDonnell <keith@dancingtext.com>
2009-04-07 11:03:19 +0200

    Tests for euclidean & pearson distances

--- recommendation_test.rb
+++ recommendation_test.rb
@@ -1,29 +1,38 @@
+require 'test/unit'
+require 'recommendation'
+require 'rubygems'
+require 'redgreen'
+
+Recommendation::DATASET = {'Lisa Rose'=> {'Lady in the Water'=> 2.5, 'Snakes on a Plane'=> 3.5,
+ 'Just My Luck'=> 3.0, 'Superman Returns'=> 3.5, 'You, Me and Dupree'=> 2.5,
+ 'The Night Listener'=> 3.0},
+'Gene Seymour'=> {'Lady in the Water'=> 3.0, 'Snakes on a Plane'=> 3.5,
+ 'Just My Luck'=> 1.5, 'Superman Returns'=> 5.0, 'The Night Listener'=> 3.0,
+ 'You, Me and Dupree'=> 3.5},
+'Michael Phillips'=> {'Lady in the Water'=> 2.5, 'Snakes on a Plane'=> 3.0,
+ 'Superman Returns'=> 3.5, 'The Night Listener'=> 4.0},
+'Claudia Puig'=> {'Snakes on a Plane'=> 3.5, 'Just My Luck'=> 3.0,
+ 'The Night Listener'=> 4.5, 'Superman Returns'=> 4.0,
+ 'You, Me and Dupree'=> 2.5},
+'Mick LaSalle'=> {'Lady in the Water'=> 3.0, 'Snakes on a Plane'=> 4.0,
+ 'Just My Luck'=> 2.0, 'Superman Returns'=> 3.0, 'The Night Listener'=> 3.0,
+ 'You, Me and Dupree'=> 2.0},
+'Jack Matthews'=> {'Lady in the Water'=> 3.0, 'Snakes on a Plane'=> 4.0,
+ 'The Night Listener'=> 3.0, 'Superman Returns'=> 5.0, 'You, Me and Dupree'=> 3.5},
+'Keith'=> {'Snakes on a Plane'=>1.5,'You, Me and Dupree'=>3.0,'Superman Returns'=>1.0}
+}
+
 class RecommendationTest < Test::Unit::TestCase
-  def setup
-    Recommendation::DATASET = {'Lisa Rose'=> {'Lady in the Water'=> 2.5, 'Snakes on a Plane'=> 3.5,
-     'Just My Luck'=> 3.0, 'Superman Returns'=> 3.5, 'You, Me and Dupree'=> 2.5,
-     'The Night Listener'=> 3.0},
-    'Gene Seymour'=> {'Lady in the Water'=> 3.0, 'Snakes on a Plane'=> 3.5,
-     'Just My Luck'=> 1.5, 'Superman Returns'=> 5.0, 'The Night Listener'=> 3.0,
-     'You, Me and Dupree'=> 3.5},
-    'Michael Phillips'=> {'Lady in the Water'=> 2.5, 'Snakes on a Plane'=> 3.0,
-     'Superman Returns'=> 3.5, 'The Night Listener'=> 4.0},
-    'Claudia Puig'=> {'Snakes on a Plane'=> 3.5, 'Just My Luck'=> 3.0,
-     'The Night Listener'=> 4.5, 'Superman Returns'=> 4.0,
-     'You, Me and Dupree'=> 2.5},
-    'Mick LaSalle'=> {'Lady in the Water'=> 3.0, 'Snakes on a Plane'=> 4.0,
-     'Just My Luck'=> 2.0, 'Superman Returns'=> 3.0, 'The Night Listener'=> 3.0,
-     'You, Me and Dupree'=> 2.0},
-    'Jack Matthews'=> {'Lady in the Water'=> 3.0, 'Snakes on a Plane'=> 4.0,
-     'The Night Listener'=> 3.0, 'Superman Returns'=> 5.0, 'You, Me and Dupree'=> 3.5},
-    'Keith'=> {'Snakes on a Plane'=>1.5,'You, Me and Dupree'=>3.0,'Superman Returns'=>1.0}
-    } unless defined?(Recommendation::DATASET)
-  end
-  
   def test_euclidean
+    assert_in_delta 0.148148148148148,
+      Recommendation.euclidean_distance('Lisa Rose', 'Gene Seymour'),
+      0.0001
   end
   
   def test_pearson
+    assert_in_delta 0.39605901719067,
+      Recommendation.pearson_distance('Lisa Rose', 'Gene Seymour'),
+      0.0001
   end
   
   def test_critic_ranking
